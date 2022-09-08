7d86dba7bebe2e0e746d82ffb21d0d4b2160a049
Lauren Newman <laurnewman1@gmail.com>
2022-09-08 16:55:12 -0400

    initialized files

---
+++ src/edu/ufl/cise/plpfa22/Lexer.java
@@ -0,0 +1,15 @@
+package edu.ufl.cise.plpfa22;
+
+public class Lexer implements ILexer{
+
+    public Lexer(String input) {
+    }
+
+    public IToken next() throws LexicalException {
+
+    }
+
+    public IToken peek() throws LexicalException {
+
+    }
+}
