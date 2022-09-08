494d80192483bb24bdecbc203f51e07078f039ff
Lauren Newman <laurnewman1@gmail.com>
2022-09-08 17:27:13 -0400

    implemented IToken

---
+++ src/edu/ufl/cise/plpfa22/Token.java
@@ -0,0 +1,38 @@
+package edu.ufl.cise.plpfa22;
+
+public class Token implements IToken{
+
+    private Kind kind;
+    private char[] text;
+    private SourceLocation source;
+
+
+    public Token(Kind kind, char[] text) {
+        this.kind = kind;
+        this.text = text;
+    }
+
+    public Kind getKind() {
+        return kind;
+    }
+
+    public char[] getText() {
+        return text;
+    }
+
+    public SourceLocation getSourceLocation() {
+        return source;
+    }
+
+    public int getIntValue() {
+        return Integer.parseInt(text.toString());
+    }
+
+    public boolean getBooleanValue() {
+        return Boolean.parseBoolean(text.toString());
+    }
+
+    public String getStringValue() {
+        return text.toString();
+    }
+}
